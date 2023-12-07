894d7c8c950b1d23c72e9fc8214efba1eaf99ac1
WooSangWook <raincoat@kakao.com>
2023-12-07 19:02:27 +0900

    Feat: config 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,10 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  }
 })
