cd67033da382f1f9990a700733f7f1dcf17018bb
WooSangWook <raincoat@kakao.com>
2023-12-07 18:38:26 +0900

    Feat: seo 추가

--- pages/index.vue
+++ pages/index.vue
@@ -6,3 +6,31 @@
       </AppAlert>
     </div>
 </template>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: 'Main page',
+  description: 'description of the main page',
+  ogTitle: 'Main page',
+  ogDescription: 'description of the main page',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: 'Main page',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
