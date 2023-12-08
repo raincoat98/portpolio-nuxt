b841ab13a536822e6f1f98398f1a21192eae977a
WooSangWook <raincoat@kakao.com>
2023-12-08 16:53:31 +0900

    Feat: test md 추가

---
+++ content/about.md
@@ -0,0 +1,5 @@
+---
+description: 'meta description'
+---
+
+# hi about
--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,16 @@
 <script lang="ts" setup>
-  definePageMeta({
-    layout: 'another'
-  })
+definePageMeta({
+  layout: 'another',
+});
 </script>
 
 <template>
   <div>
     this is about page
+    <article class="prose dark:prose-invert prose-h1:text-2xl">
+      <ContentDoc path="/about" />
+    </article>
   </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
