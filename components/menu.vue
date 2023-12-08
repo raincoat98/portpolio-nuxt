42a50906183251b8c0f455feb5cb34dbce68ad07
WooSangWook <raincoat@kakao.com>
2023-12-08 17:37:26 +0900

    Feat: queryContent test 추가

--- components/menu.vue
+++ components/menu.vue
@@ -12,6 +12,9 @@
       <nuxt-link to="/projects" class="mr-4 text-sm hover:bg-gray-200 p-2">
         Project
       </nuxt-link>
+      <nuxt-link to="/blog" class="mr-4 text-sm hover:bg-gray-200 p-2">
+        Blogs
+      </nuxt-link>
     </nav>
   </div>
 </template>
