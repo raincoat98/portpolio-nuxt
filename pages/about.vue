8a1e97bb4ef442f162784747efbb7dbfbd19a2f3
WooSangWook <raincoat@kakao.com>
2023-12-08 17:00:08 +0900

    Fix: about 페이지 추가

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,11 @@
 <script lang="ts" setup>
-definePageMeta({
-  layout: 'another',
+({
+  layout: 'default',
 });
 </script>
 
 <template>
   <div>
-    this is about page
     <article class="prose dark:prose-invert prose-h1:text-2xl">
       <ContentDoc path="/about" />
     </article>
