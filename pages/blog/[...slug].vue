c333929fbb576a34884e8a8acd542c6e3a6d0acc
WooSangWook <raincoat@kakao.com>
2023-12-12 15:26:02 +0900

    Feat: error 페이지 추가

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,18 +2,24 @@
   <article
     class="prose dark:prose-invert max-w-none prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
   >
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-16">
-        <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
-          <ContentRenderer :value="doc" />
+    <ContentDoc>
+      <template #not-found>
+        <h1>Document not found (404)</h1>
+        <p>This blog post could not be found.</p>
+      </template>
+      <template v-slot="{ doc }">
+        <div class="grid grid-cols-6 gap-16">
+          <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+            <ContentRenderer :value="doc" />
+          </div>
+          <div class="col-span-2 not-prose" v-if="doc.toc">
+            <aside class="sticky top-8">
+              <div class="font-semibold mb-2">Table of Content</div>
+              <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+            </aside>
+          </div>
         </div>
-        <div class="col-span-2 not-prose" v-if="doc.toc">
-          <aside class="sticky top-8">
-            <div class="font-semibold mb-2">Table of Content</div>
-            <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
-          </aside>
-        </div>
-      </div>
+      </template>
     </ContentDoc>
   </article>
 </template>
@@ -22,7 +28,6 @@ const activeId = ref(null);
 
 onMounted(() => {
   const callback = entries => {
-    console.log(entries);
     for (const entry of entries) {
       if (entry.isIntersecting) {
         activeId.value = entry.target.id;
@@ -43,11 +48,5 @@ onMounted(() => {
       observer.unobserve(element);
     }
   });
-});
-
-onBeforeUnmount(() => {
-  for (const el of elements) {
-    observer.unobserve(el);
-  }
 });
 </script>
